9ef17a8bb1a2eaa908bf987cfedf6cd7339c09b6
Tyler Bray <broncobray77@gmail.com>
2018-03-07 14:57:52 -0800

    added psudeoclassical style

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,25 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.count = 0,
+  this.start =0
 };
 
+Queue.prototype.enqueue = function(value) {
+  this[this.count] = value;
+  this.count++;
+}
+
+Queue.prototype.dequeue = function() {
+  if ( this.count - this.start > 0) {
+    var temp = this[this.start];
+    delete this[this.start];
+    this.start++;
+    return temp;
+  }
+}
+
+Queue.prototype.size = function() {
+  return this.count - this.start;
+}
 
