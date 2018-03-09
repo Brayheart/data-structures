425e77577c05fd8d8dfed6f777cca2c33d42da82
Tyler Bray <broncobray77@gmail.com>
2018-03-09 10:15:16 -0800

    added linked list and hash table

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,23 +1,50 @@
-
-
-var HashTable = function() {
+var HashTable = function () {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
+
 };
 
-HashTable.prototype.insert = function(k, v) {
+HashTable.prototype.insert = function (k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var currentValue = this._storage.get(index);
+  if (currentValue === undefined) {
+    this._storage.set(index, [[k, v]]);
+  } else {
+    for (var i = 0; i < currentValue.length; i++) {
+      if (currentValue[i][0] === k) {
+        currentValue[i] = [k, v];
+      } else {
+        this._storage.get(index).push([k, v]);
+      }
+    }
+  }
+
 };
 
-HashTable.prototype.retrieve = function(k) {
+HashTable.prototype.retrieve = function (k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var current = this._storage.get(index);
+
+  for (var i = 0; i < current.length; i++) {
+    if (current[i][0] === k) {
+      return current[i][1];
+    }
+  }
 };
 
-HashTable.prototype.remove = function(k) {
+HashTable.prototype.remove = function (k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var current = this._storage.get(index);
+
+  for (var i = 0; i < current.length; i++) {
+    if (current[i][0] === k) {
+      current.splice(i, 1);
+      this._storage.set(index, current);
+      break;
+    }
+  }
+
 };
-
-
 
 /*
  * Complexity: What is the time complexity of the above functions?
--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,21 +1,42 @@
-var LinkedList = function() {
+var LinkedList = function () {
   var list = {};
   list.head = null;
   list.tail = null;
 
-  list.addToTail = function(value) {
+  list.addToTail = function (value) {
+    var newNode = new Node(value);
+    if (this.head === null) {
+      this.head = newNode;
+    }
+    if (this.tail) {
+      this.tail.next = newNode;
+    };
+    this.tail = newNode;
   };
 
-  list.removeHead = function() {
+  list.removeHead = function () {
+    var temp = this.head;
+    delete this.head;
+    list.head = temp.next
+    return temp.value;
   };
 
-  list.contains = function(target) {
+  list.contains = function (target) {
+    var currentNode = this.head;
+    while (currentNode !== null) {
+      if (target === currentNode.value) {
+        return true;
+      }
+      currentNode = currentNode.next;
+    }
+    return false;
+
   };
 
   return list;
 };
 
-var Node = function(value) {
+var Node = function (value) {
   var node = {};
 
   node.value = value;
@@ -23,7 +44,6 @@ var Node = function(value) {
 
   return node;
 };
-
 /*
  * Complexity: What is the time complexity of the above functions?
  */
